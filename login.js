a448d6b3101e0bedf81e65f5d1aaaf99d12b791d
Aman Kumar <amanthecoder001@gmail.com>
2026-07-21 12:00:00 +0530

    completed login page

---
+++ login.js
@@ -0,0 +1,49 @@
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { showToast } from "./toast.js";
+
+const loginForm = document.getElementById("loginForm");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const remember = document.getElementById("remember");
+
+onAuthStateChanged(auth, (user) => {
+
+    if (user) {
+        window.location.replace("dashboard.html");
+    }
+
+});
+
+loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const userEmail = email.value;
+    const userPassword = password.value;
+
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, userEmail, userPassword);
+        showToast("Login Successful", "success");
+        console.log(userCredential.user);
+        
+        setTimeout(() => {
+            window.location.href = "dashboard.html";
+        }, 2000);
+
+    } catch (error) {
+
+        if (error.code === "auth/invalid-credential") {
+            showToast("Invalid Email or Password", "error");
+
+        } else if (error.code === "auth/invalid-email") {
+            showToast("Please enter a valid email.", "error");
+        } else {
+
+            alert(error.message);
+
+        }
+
+    }
+
+});
+
